5bca68aad9f98c2267d70a2c1936f7780b60c6da
Kandarp Kakkad <kandarp.b.kakkad@gmail.com>
2020-11-27 00:05:00 +0530

    26-11-2020

--- dashboard.py
+++ dashboard.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 home_app = Flask(__name__)
 
+
 @home_app.route("/", methods=['GET'])
 def home():
     return render_template("home.html")
 
+
 if __name__ == "__main__":
-    home_app.run(debug=True, port=6060)
+    home_app.run(debug=True, port=7000)
