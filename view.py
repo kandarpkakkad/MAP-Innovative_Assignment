5890577808e8a3eace57846cecdcc55885b9d836
Kandarp Kakkad <kandarp.b.kakkad@gmail.com>
2020-11-28 22:16:44 +0530

    Version 1

--- view.py
+++ view.py
@@ -1,26 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import pymysql as sql
+import os
+import sys
+import datetime
+import ast
 
 view_app = Flask(__name__)
 
 
 @view_app.route("/", methods=["GET"])
-def take_get():
-    data = dict()
-    data["view_api"] = True
-    data["method"] = "get"
-    data["port"] = 4000
-    data["returns"] = "dictionary"
-    return render_template("view.html", data=data)
+def view_get():
+    global role, result, username, login
+    typ = 0
+    rol = ''
+    at = []
+    sear_sub = ''
+    role = request.cookies.get('role')
+    if 'result' in request.cookies:
+        result = request.cookies.get('result')
+        result = ast.literal_eval(result)
+        return render_template('home/view_attendance.html', data = {'role': role, 'result': result['name'], 'search_set': at})
+    else:
+        return redirect("http://localhost:9000/", code=302)
 
 
 @view_app.route("/", methods=["POST"])
 def take_post():
-    data = dict()
-    data["view_api"] = True
-    data["method"] = "post"
-    data["port"] = 4000
-    data["returns"] = "dictionary"
-    return render_template("view.html", data=data)
+    global role, result, username, login
+    typ = 0
+    rol = ''
+    at = []
+    sear_sub = ''
+    role = request.cookies.get('role')
+    if 'result' in request.cookies:
+        sear_sub = request.form.get('search_sub')
+        lt = request.form.get('ltype')
+        result = request.cookies.get('result')
+        result = ast.literal_eval(result)
+        rol = result['roll_number']
+        if lt == 'lecture':
+            typ = 0
+        elif lt == 'lab':
+            typ = 1
+        else:
+            typ = 2
+        #at = att.objects.filter(lecture=sear_sub, lecture_type=typ, roll_number=rol)
+        search_att = "SELECT * FROM attendance WHERE lecture='"+sear_sub+"' AND lecture_type='"+typ+"' AND roll_number='"+rol+"';"
+        cursor.execute(search_att)
+        stu = cursor.fetchall()
+        result = request.cookies.get('result')
+        result = ast.literal_eval(result)
+        return render_template('home/view_attendance.html', data={'role': role, 'result': result['name'], 'search_set': stu})
+    else:
+        return redirect("http://localhost:9000/", code=302)
 
 
 if __name__ == "__main__":
